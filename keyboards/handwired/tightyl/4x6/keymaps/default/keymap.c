e04c9d086e2eab8c7b45612522331e745cf952f3
Cliff Kelley <cliff@bestdealspring.com>
2022-04-04 16:11:34 -0600

    Fix numpad 5

--- keyboards/handwired/tightyl/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/tightyl/4x6/keymaps/default/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 */
     [_SYMNUM] = LAYOUT(
         KC_EQL,  KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR,              		     KC_PSLS, KC_P7,   KC_P8,   KC_P9,   _______, KC_MINS,
-        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, _______,               		 KC_PAST, KC_P4,   KC_P6,   KC_P6,   KC_PPLS, _______,
+        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, _______,               		 KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
         KC_LSFT, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, _______,               		 KC_NUM,  KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______,
                           _______, _______, _______, _______, _______,		_______, KC_DEL,  _______, KC_P0,   KC_PDOT
 	),
