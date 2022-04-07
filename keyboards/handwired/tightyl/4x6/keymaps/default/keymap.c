31b1aba34d88783e207ffc69754008277cd49016
Cliff Kelley <cliff@bestdealspring.com>
2022-04-07 15:20:38 -0600

    Add steno layer

--- keyboards/handwired/tightyl/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/tightyl/4x6/keymaps/default/keymap.c
@@ -1,22 +1,17 @@
 #include QMK_KEYBOARD_H
+#include "keymap_steno.h"
 
 #define _BASE 0
 #define _SYMNUM 1
 #define _SPEC 2
 #define _ARROWS 3
 #define _FUNC 4
+#define _STENO 5
 
 #define SFT_ESC  SFT_T(KC_ESC)
 #define CTL_BSPC CTL_T(KC_BSPC)
 #define ALT_SPC  ALT_T(KC_SPC)
 #define SFT_ENT  SFT_T(KC_ENT)
-
-#define KC_ML KC_MS_LEFT
-#define KC_MR KC_MS_RIGHT
-#define KC_MU KC_MS_UP
-#define KC_MD KC_MS_DOWN
-#define KC_MB1 KC_MS_BTN1
-#define KC_MB2 KC_MS_BTN1
 
 #define SYMNUM LT(_SYMNUM, KC_SPC)
 #define SPEC LT(_SPEC, KC_ESC)
@@ -24,20 +19,29 @@
 #define R_ARRW LT(_ARROWS, KC_O)
 #define ENT_FN LT(_FUNC, KC_ENT)
 #define CTL_QU RCTL_T(KC_QUOT)
+#define TO_BSE TO(_BASE)
+#define TO_STN TO(_STENO)
 
 const rgblight_segment_t PROGMEM my_default_layer[] = RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_BLACK});
 const rgblight_segment_t PROGMEM symnum_layer[] = RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_RED});
 const rgblight_segment_t PROGMEM spec_layer[] = RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_BLUE});
 const rgblight_segment_t PROGMEM arrows_layer[] = RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_GREEN});
 const rgblight_segment_t PROGMEM func_layer[] = RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_PURPLE});
+const rgblight_segment_t PROGMEM steno_layer[] = RGBLIGHT_LAYER_SEGMENTS({0,1,HSV_TEAL});
 
 const rgblight_segment_t* const PROGMEM my_rgblight_layers[] = RGBLIGHT_LAYERS_LIST(
 	my_default_layer,
 	symnum_layer,
 	spec_layer,
 	arrows_layer,
-	func_layer
+	func_layer,
+	steno_layer
 );
+
+void eeconfig_init_user()
+{
+	steno_set_mode(STENO_MODE_GEMINI);
+}
 
 void keyboard_post_init_user(void)
 {
@@ -83,19 +87,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Specials
      * +-----------------------------------------+                             +-----------------------------------------+
-     * |      |      |      |      |      |      |                             | VOL+ |      | CALC | PRSCR|      |      |
+     * |      |      |      |      |      |      |                             | VOL+ |      | CALC | PRSCR|      | STENO|
      * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
      * |      |      |      |      |      |      |                             | HOME | PGDN | PGUP |  END | CAPS |      |
      * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
-     * | RESET|      |      |      |      |      |                             | VOL- | PREV | PLAY | NEXT | SCRLK| RESET|
+     * |      |      |      |      |      |      |                             | VOL- | PREV | PLAY | NEXT | SCRLK|      |
      * +------+------+------+------+-------------+------+               +------+-------------+------+------+------+------+
      *               |      |      |      |      |      |               |      |      |      |      |      |
      *               +-------------+-------------+------+               +------+-------------+-------------+
 	 */
     [_SPEC] = LAYOUT(
-		_______, _______, _______, _______, _______, _______,              			 KC_VOLU, _______, KC_CALC, KC_PSCR, _______, _______,
+		_______, _______, _______, _______, _______, _______,              			 KC_VOLU, _______, KC_CALC, KC_PSCR, _______, TO_STN,
         _______, _______, _______, _______, _______, _______,               		 KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_CAPS, _______,
-        QK_BOOT, _______, _______, _______, _______, _______,               		 KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, KC_SCRL, QK_BOOT,
+        _______, _______, _______, _______, _______, _______,               		 KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, KC_SCRL, _______,
                        	  _______, _______, _______, _______, _______,		_______, _______, _______, _______, _______
 	),
 
@@ -105,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
      * |      |      | LEFT | DOWN | RIGHT|      |                             | LEFT | DOWN |  UP  | RIGHT|      |      |
      * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
-     * |      |      |      |      |      |      |                             |      |      |      |      |      |      |
+     * | RESET|      |      |      |      |      |                             |      |      |      |      |      | RESET|
      * +------+------+------+------+-------------+------+               +------+-------------+------+------+------+------+
      *               |      |      |      |      |      |               |      |      |      |      |      |
      *               +-------------+-------------+------+               +------+-------------+-------------+
@@ -113,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ARROWS] = LAYOUT(
 		_______, _______, _______, KC_UP,   _______, _______,             			 _______, _______, _______, _______, _______, _______,
         _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______,             			 KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
-        _______, _______, _______, _______, _______, _______,             			 _______, _______, _______, _______, _______, _______,
+        QK_BOOT, _______, _______, _______, _______, _______,             			 _______, _______, _______, _______, _______, QK_BOOT,
                     	  _______, _______, _______, _______, _______,		_______, _______, _______, _______, _______
 	),
 
@@ -133,6 +137,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,               		 _______, KC_F4,   KC_F5,   KC_F6,   KC_F11, _______,
         _______, _______, _______, _______, _______, _______,               		 _______, KC_F1,   KC_F2,   KC_F3,   KC_F12, _______,
             		   	  _______, _______, _______, _______, _______,		_______, _______, _______, _______, _______
+	),
+
+    /* Stenography
+     * +-----------------------------------------+                             +-----------------------------------------+
+     * |  #1  |  #2  |  #3  |  #4  |  #5  |  #6  |                             |  #7  |  #8  |  #9  |  #A  |  #B  |  #C  |
+     * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+     * |  FN  |      |   T  |   P  |   H  |      |                             |      |   F  |   P  |   L  |   T  |   D  |
+     * |------+   S  +------+------+------+   *  |                             |   *  +------+------+------+------+------|
+     * |      |      |   K  |   W  |   R  |      |                             |      |   R  |   B  |   G  |   S  |   Z  |
+     * +------+------+------+------+-------------+------+               +------+-------------+------+------+------+------+
+     *               | BASE |      |   A  |   O  |      |               |      |   E  |   U  | RES1 | RES2 |
+     *               +-------------+-------------+------+               +------+-------------+-------------+
+	 */
+    [_STENO] = LAYOUT(
+		STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,              			 STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC,
+		STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1,              			 STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
+        _______, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2,               		 STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
+            		   	  TO_BSE, _______,  STN_A,   STN_O,   _______,		_______, STN_E,   STN_U,   STN_RE1, STN_RE2
 	)
 };
 
@@ -153,5 +175,6 @@ layer_state_t layer_state_set_user(layer_state_t state)
 	rgblight_set_layer_state(2, layer_state_cmp(state, _SPEC));
 	rgblight_set_layer_state(3, layer_state_cmp(state, _ARROWS));
 	rgblight_set_layer_state(4, layer_state_cmp(state, _FUNC));
+	rgblight_set_layer_state(5, layer_state_cmp(state, _STENO));
 	return state;
 }
